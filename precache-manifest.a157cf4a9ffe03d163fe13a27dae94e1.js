da0cb353dd722366a4fa96f6a32149d7b692e28a
CraigWhittington <cfwhit@hotmail.com>
2019-11-04 17:06:08 +0000

    Updates

---
+++ precache-manifest.a157cf4a9ffe03d163fe13a27dae94e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52a807c9433db5ffac395ec5d5aa19fb",
+    "url": "/roboreact/index.html"
+  },
+  {
+    "revision": "c2b551cf4e8f502d8038",
+    "url": "/roboreact/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "20ebaac2c676d9d1aba7",
+    "url": "/roboreact/static/css/main.fe582c14.chunk.css"
+  },
+  {
+    "revision": "c2b551cf4e8f502d8038",
+    "url": "/roboreact/static/js/2.c11df684.chunk.js"
+  },
+  {
+    "revision": "20ebaac2c676d9d1aba7",
+    "url": "/roboreact/static/js/main.740c7472.chunk.js"
+  },
+  {
+    "revision": "a1db30e6febc5cc94905",
+    "url": "/roboreact/static/js/runtime-main.fcc0899d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboreact/static/media/SEGA.12a3db8b.woff"
+  }
+]);
